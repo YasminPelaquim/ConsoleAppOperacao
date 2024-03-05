2874202754da81c2e8a5a45d17753ff26264091d
Usuario <Usuario@CPD02-M06>
2024-03-05 12:21:36 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppOperacao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOperacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int pnu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int snu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha uma operação: (*, /, +, -) ");
+            string ope = Console.ReadLine();
+
+            if (ope == "+")
+            {
+                int result = pnu + snu;
+                Console.WriteLine("O resultado é: " + result);
+            } else if (ope == "-")
+            {
+                int result = pnu - snu;
+                Console.WriteLine("O resultado é: " + result);
+            } else if (ope == "*")
+            {
+                int result = pnu * snu;
+                Console.WriteLine("O resultado é: " + result);
+            } else if (ope == "/")
+            {
+                int result = pnu / snu;
+                Console.WriteLine("O resultado é: " + result);
+            } else if (ope != "*, /, +, -")
+            {
+                Console.WriteLine("A operação não pode ser efetuada");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
